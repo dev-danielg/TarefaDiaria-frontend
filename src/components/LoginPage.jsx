6e71d4cdd2d5ed55fd23f885d7cfb5153caf8118
lucassant <lucasfdesantana095@gmail.com>
2026-07-14 22:49:58 -0300

    feat: adicionando form simples para teste

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,13 @@
 function LoginPage(){
     return(
         <>
+        <section class='w-full h-screen border flex items-center justify-center'>
+            <form method='' class='border w-1/2 h-1/2 flex flex-col items-center justify-center gap-4'>
+                <input type='email' placeholder='informe seu e-mail' class='p-2'/>
+                <input type='password'placeholder='informe sua senha' class='p-2'/>
+                <button class='p-2 border rounded-xl'>acessar</button>
+            </form>
+        </section>
         </>
     )
 }
