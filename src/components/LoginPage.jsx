00589da82e62b5d6a459cb0cf47f56ad4805f42a
lucassant <lucasfdesantana095@gmail.com>
2026-07-17 11:55:14 -0300

    refactor: refatorando componentes de login e cadastro

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import axios from 'axios'
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 function LoginPage(){
 
-    function NewUser(){
-        alert('Redirecionando...')
+    const navigate = useNavigate();
+    function ToNewUser(){
+        navigate('/NewUserPage')
     }
 
     const [email, setEmail] = useState('')
@@ -37,13 +39,13 @@ function LoginPage(){
 
     return(
         <>
-        <section class='w-full h-screen border flex flex-col items-center justify-center'>
-            <h1 class='text-6xl font-semibold mb-12'>Tarefa Diária</h1>
-            <form onSubmit={handleCadastro} class='border w-1/2 h-1/2 flex flex-col items-center justify-center gap-4'>
+        <section className='w-full h-screen border flex flex-col items-center justify-center'>
+            <h1 className='text-6xl font-semibold mb-12'>Tarefa Diária</h1>
+            <form onSubmit={handleCadastro} className='border w-1/2 h-1/2 flex flex-col items-center justify-center gap-4'>
                 <input 
                     type='email' 
                     placeholder='informe seu e-mail' 
-                    class='p-2 bg-gray-200 border border-gray-400 rounded-md'
+                    className='p-2 bg-gray-200 border border-gray-400 rounded-md'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
@@ -51,20 +53,20 @@ function LoginPage(){
                 <input 
                     type='password'
                     placeholder='informe sua senha' 
-                    class='p-2 bg-gray-200 border border-gray-400 rounded-md'
+                    className='p-2 bg-gray-200 border border-gray-400 rounded-md'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-                {error && <p class='text-red-500'>{error}</p>}
+                {error && <p className='text-red-500'>{error}</p>}
                 <button 
                     type='submit'
-                    class='p-2 w-48 border border-gray-400 rounded-md hover:bg-gray-800 hover:text-gray-200 font-semibold'
+                    className='p-2 w-48 border border-gray-400 rounded-md hover:bg-gray-800 hover:text-gray-200 font-semibold'
                     disabled={loading}
                 >
                     {loading ? 'Acessando...' : 'Acessar'}
                 </button>
-                <p class='text-gray-400 font-semibold'>Não tem conta? <span class='text-gray-800 font-semibold cursor-pointer' onClick={NewUser}>Clique aqui!</span></p>
+                <p className='text-gray-400 font-semibold'>Não tem conta? <span className='text-gray-800 font-semibold cursor-pointer' onClick={ToNewUser}>Clique aqui!</span></p>
             </form>
         </section>
         </>
--- src/components/NewUserPage.jsx
+++ src/components/NewUserPage.jsx
@@ -1,9 +1,17 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
 function NewUserPage(){
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
+
+    const navigate = useNavigate();
+    function ReturnLogin(){
+        navigate('/')
+    }
 
     const handleCadastro = async (e) => {
         e.preventDefault()
@@ -32,13 +40,13 @@ function NewUserPage(){
 
     return(
         <>
-        <section class='w-full h-screen border flex flex-col items-center justify-center'>
-            <h1 class='text-6xl font-semibold mb-12'>Tarefa Diária</h1>
-            <form onSubmit={handleCadastro} class='border w-1/2 h-1/2 flex flex-col items-center justify-center gap-4'>
+        <section className='w-full h-screen border flex flex-col items-center justify-center'>
+            <h1 className='text-6xl font-semibold mb-12'>Tarefa Diária</h1>
+            <form onSubmit={handleCadastro} className='border w-1/2 h-1/2 flex flex-col items-center justify-center gap-4'>
                 <input 
                     type='text' 
                     placeholder='nome' 
-                    class='p-2 bg-gray-200 border border-gray-400 rounded-md'
+                    className='p-2 bg-gray-200 border border-gray-400 rounded-md'
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     required
@@ -46,7 +54,7 @@ function NewUserPage(){
                 <input 
                     type='email' 
                     placeholder='e-mail' 
-                    class='p-2 bg-gray-200 border border-gray-400 rounded-md'
+                    className='p-2 bg-gray-200 border border-gray-400 rounded-md'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
@@ -54,19 +62,20 @@ function NewUserPage(){
                 <input 
                     type='password'
                     placeholder='senha' 
-                    class='p-2 bg-gray-200 border border-gray-400 rounded-md'
+                    className='p-2 bg-gray-200 border border-gray-400 rounded-md'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-                {error && <p class='text-red-500'>{error}</p>}
+                {error && <p className='text-red-500'>{error}</p>}
                 <button 
                     type='submit'
-                    class='p-2 w-48 border border-gray-400 rounded-md hover:bg-gray-800 hover:text-gray-200 font-semibold'
+                    className='p-2 w-48 border border-gray-400 rounded-md hover:bg-gray-800 hover:text-gray-200 font-semibold'
                     disabled={loading}
                 >
                     {loading ? 'Cadastrando...' : 'Cadastrar'}
                 </button>
+                <p className='text-gray-800 font-semibold cursor-pointer' onClick={ReturnLogin}> Retornar</p>
             </form>
         </section>
         </>
