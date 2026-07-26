da6a9e72e5383d443f84a050035dbe26b26f4fa8
lucassant <lucasfdesantana095@gmail.com>
2026-07-26 18:34:41 -0300

    style: estilizando componente de cadastro e login

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,7 +4,9 @@ function MainPage() {
 
   return (
     <>
-      <h1>Funcionando!</h1>
+      <section className='w-full h-screen flex flex-col items-center justify-center bg-sky-600'>
+        <h1>Funcionando!</h1>
+      </section>
     </>
   )
 }
