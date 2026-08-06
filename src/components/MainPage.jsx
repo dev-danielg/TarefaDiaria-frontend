fba491561d581726bf11b34f81ddf8e8545a071d
lucassant <lucasfdesantana095@gmail.com>
2026-08-06 00:35:54 -0300

    feat: adicionando toggle para tarefas

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,40 +1,61 @@
 import { useState } from 'react'
-import {Eraser} from 'lucide-react';
+import {Eraser, X} from 'lucide-react';
 
 function MainPage() {
 
-  const [task, setTask] = useState('')
+  const [tarefa, setTarefa] = useState('');
 
-  function capturarCaracter(){
-    const taskCapturada = task.target.value
-    SetTask(taskCapturada)
-    console.log(taskCapturada)
-  }
-
-  function LimparCampo(){
-    return Set
-
-  }
+  const [mostrarToggle, setMostrarToggle] = useState(false)
 
   return (
     <>
       <section className='w-full h-screen flex flex-col items-center justify-center bg-sky-600'>
         <div className='relative px-8'>
-          <input type='text' placeholder='Procure pela task' className='pr-10 w-64 bg-slate-400 p-2' value={task} onChange={(e) => setTask(e.target.value)}/>
+          <input type='text' placeholder='Procure pela tarefa' className='pr-10 w-64 bg-slate-100 p-2'/>
           <Eraser 
           className='absolute top-1/2 translate-x-1/2 -translate-y-1/2 right-14 text-blue-600 cursor-pointer'
           size={18}
-
           />
         </div>
-        <div className='border w-full h-1/2 flex items-center justify-evenly'>
-          <div className=" isolate aspect-video w-92 h-44 bg-white/20 shadow-lg rounded-xl">
-            <div className='w-10 h-10 bg-slate-200 rounded-full shadow-xl cursor-pointer hover transform'>
-            <span>+</span>
-            </div>
+        <button  onClick={() => setMostrarToggle(!mostrarToggle)} className='w-64 p-2 mt-6 bg-sky-400 font-semibold cursor-pointer transition duration-200 hover:shadow-xl'>
+            Nova Tarefa
+        </button>
+
+        {mostrarToggle && 
+        <div 
+        className='w-96 px-2 h-96 border border-gray-400 bg-slate-100 rounded-xl flex flex-col gap-2 items-center justify-center absolute inset-y' >
+            <span className='w-full ' onClick={() => setMostrarToggle(!mostrarToggle)}>
+              <X
+              className='text-red-400 cursor-pointer transition duration-250 hover:text-red-500'
+              />
+            </span>
+            <label className='font-semibold w-full border'>
+              Título
+            </label>
+            <input type='text'
+            required
+            className='w-full p-2 border border-gray-400 bg-gray-200 rounded-xl transition duration-150 focus:outline-none focus:ring-2 focus:ring-blue-600' />
+
+            <label className='font-semibold w-full border mt-4'>
+              Descrição
+            </label>
+            <textarea className='h-fit p-2 border border-gray-400 bg-gray-200 rounded-xl w-full transition duration-150 focus:outline-none focus:ring-2 focus:ring-blue-600'></textarea>
+
+            <button className='w-64 p-2 mt-6 bg-sky-400 font-semibold cursor-pointer rounded-xl transition duration-200 hover:shadow-xl' type='submit'>
+                Adicionar
+            </button>
+        </div>
+        }
+        {/* <div className='border w-full h-1/2 flex items-center justify-evenly'>
+
+          <div className="border isolate aspect-video w-92 h-44 bg-white/20 shadow-lg rounded-xl">
+          </div>
+          <div className="border isolate aspect-video w-92 h-44 bg-white/20 shadow-lg rounded-xl">
+          </div>
+          <div className="border isolate aspect-video w-92 h-44 bg-white/20 shadow-lg rounded-xl">
           </div>
           
-        </div>
+        </div> */}
       </section>
     </>
   )
