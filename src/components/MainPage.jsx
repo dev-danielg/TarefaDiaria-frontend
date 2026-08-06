c1bdc9f5f4731ee10f61954ed2bf03493f0349e8
lucassant <lucasfdesantana095@gmail.com>
2026-08-05 23:40:39 -0300

    atualizacoes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Eraser} from 'lucide-react';
+import {Eraser, Plus} from 'lucide-react';
 
 function MainPage() {
 
@@ -12,26 +12,30 @@ function MainPage() {
   }
 
   function LimparCampo(){
-    return Set
-
+    return setTask('')
   }
 
   return (
     <>
       <section className='w-full h-screen flex flex-col items-center justify-center bg-sky-600'>
-        <div className='relative px-8'>
-          <input type='text' placeholder='Procure pela task' className='pr-10 w-64 bg-slate-400 p-2' value={task} onChange={(e) => setTask(e.target.value)}/>
-          <Eraser 
-          className='absolute top-1/2 translate-x-1/2 -translate-y-1/2 right-14 text-blue-600 cursor-pointer'
-          size={18}
-
-          />
+          <button className='border bg-cyan-300 h-10 pl-4 pr-10 flex items-center justify-evenly'>
+              Nova Tarefa
+              <Plus
+              className='ml-4'
+              size={16}
+              />
+          </button>
+          <div className='relative px-8'>
+            <input type='text' placeholder='Procure pela task' className='pr-10 w-90 bg-slate-200 p-2' value={task} onChange={(e) => setTask(e.target.value)}/>
+            <Eraser 
+            className='absolute top-1/2 translate-x-1/2 -translate-y-1/2 right-14 text-gray-400 cursor-pointer transition duration-150 hover:text-sky-600'
+            size={18}
+            onClick={LimparCampo}
+            />
         </div>
         <div className='border w-full h-1/2 flex items-center justify-evenly'>
-          <div className=" isolate aspect-video w-92 h-44 bg-white/20 shadow-lg rounded-xl">
-            <div className='w-10 h-10 bg-slate-200 rounded-full shadow-xl cursor-pointer hover transform'>
-            <span>+</span>
-            </div>
+
+          <div className="isolate aspect-video w-92 h-64 bg-white/20 shadow-lg rounded-xl">
           </div>
           
         </div>
